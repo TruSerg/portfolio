8e7977a9b4d7fde4b943d0b9826002a215967126
Sergey <truhan_1984@mail.ru>
2023-06-09 11:52:15 +0300

    fixed layout

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,19 @@
 const burger = document.querySelector(".icon-menu");
 const menu = document.querySelector(".menu");
 const menuLinks = menu.querySelectorAll(".menu-list__item");
+const body = document.querySelector(".body");
 
 burger.addEventListener("click", () => {
   menu.classList.toggle("_active");
   burger.classList.toggle("_active");
+  body.classList.toggle("lock");
 });
 
 menuLinks.forEach((link) => {
   link.addEventListener("click", () => {
     menu.classList.remove("_active");
     burger.classList.remove("_active");
+    body.classList.remove("lock");
   });
 });
 
@@ -18,5 +21,6 @@ document.addEventListener("click", (e) => {
   if (!(e.target.closest(".menu") || e.target.closest(".icon-menu"))) {
     menu.classList.remove("_active");
     burger.classList.remove("_active");
+    body.classList.remove("lock");
   }
 });
