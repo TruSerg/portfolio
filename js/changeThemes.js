17466209cb0daf02e94e9f14e1f081fb84edc965
Sergey <truhan_1984@mail.ru>
2023-09-11 12:41:04 +0300

    fixed body layout

--- js/changeThemes.js
+++ js/changeThemes.js
@@ -1,30 +1,30 @@
 const changeThemes = () => {
-  const btnChangeWhite = document.querySelector(".top__button-color--white");
-  const btnChangeBlack = document.querySelector(".top__button-color--black");
-  const body = document.body;
+	const btnChangeWhite = document.querySelector('.top__button-color--white');
+	const btnChangeBlack = document.querySelector('.top__button-color--black');
+	const body = document.body;
 
-  const state = {
-    color: body.classList.contains("white") ? "white" : "black",
-  };
+	const state = {
+		color: body.classList.contains('white') ? 'white' : 'black',
+	};
 
-  const changeToBlack = () => {
-    if (state.color !== "black") {
-      body.classList.add("black");
-      body.classList.remove("white");
-      state.color = "black";
-    }
-  };
+	const changeToBlack = () => {
+		if (state.color !== 'black') {
+			body.classList.add('black');
+			body.classList.remove('white');
+			state.color = 'black';
+		}
+	};
 
-  const changeToWhite = () => {
-    if (state.color !== "white") {
-      body.classList.add("white");
-      body.classList.remove("black");
-      state.color = "white";
-    }
-  };
+	const changeToWhite = () => {
+		if (state.color !== 'white') {
+			body.classList.add('white');
+			body.classList.remove('black');
+			state.color = 'white';
+		}
+	};
 
-  btnChangeWhite.addEventListener("click", changeToWhite);
-  btnChangeBlack.addEventListener("click", changeToBlack);
+	btnChangeWhite.addEventListener('click', changeToWhite);
+	btnChangeBlack.addEventListener('click', changeToBlack);
 };
 
 changeThemes();
